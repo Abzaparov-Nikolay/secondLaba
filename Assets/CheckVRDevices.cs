f441f7a0de325b999efa2084c37e2972226cd9ac
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2023-03-21 18:05:27 +0500

    Some changes

--- Assets/CheckVRDevices.cs
+++ Assets/CheckVRDevices.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class CheckVRDevices : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     [System.Obsolete]
     void Update()
     {
         var inputDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevices(inputDevices);
 
-        var desireCharracteristics = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand| UnityEngine.XR.InputDeviceCharacteristics.Left| UnityEngine.XR.InputDeviceCharacteristics.Controller;
+        var desireCharracteristics = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand
+            | UnityEngine.XR.InputDeviceCharacteristics.Left
+            | UnityEngine.XR.InputDeviceCharacteristics.Controller;
         UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(desireCharracteristics, inputDevices);
-        foreach(var device in inputDevices){
-            if(device.name != "Head Tracking - OpenXR"){
-            //Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-
+        foreach (var device in inputDevices)
+        {
+            if (device.name != "Head Tracking - OpenXR")
+            {
+                //Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
             }
-            
         }
     }
 }
